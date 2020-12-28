0abc69a8ce2102c06b8d23a96c9c42d6f26f6644
Przemysław Hinca <pshemekhinca@gmail.com>
2020-12-28 14:40:27 +0100

    Additional random list of hero items.

--- heroes.py
+++ heroes.py
@@ -1,29 +1,38 @@
 import random
+import json
 
 
-def get_name():
-    names = open('txt_files/name_dwarf.txt').read().splitlines()
-    name = random.choice(names)
-    return name
+def get_name(race):
+    with open(f'txt_files/name_{race}.txt') as f:
+        names = f.read().splitlines()
+        name = random.choice(names)
+        return name
+
+def get_hero_items(distance):
+    with open(f'txt_files/{distance}_weapon.txt') as f:
+        weapons = f.read().splitlines()
+        weapon = random.sample(weapons, 3)
+        return list(weapon)
 
 
 class Hero:
-    def __init__(self, name: str, power=10, agile=10, armor=10, iq=10, intuition=10, charisma=10):
+    def __init__(self, name: str, power=10, agile=10, durability=10, iq=10, intuition=10, charisma=10):
         self.name = name
         self.power = power
         self.agile = agile
-        self.armor = armor
+        self.durability = durability
         self.iq = iq
         self.intuition = intuition
         self.charisma = charisma
         self.items: list[str] = []
 
+
     def get_hero(self):
-        hero = {'name': self.name, 'power': self.power, 'agile': self.agile, 'armor': self.armor, 'iq': self.iq,
-                'intuition': self.intuition, 'charisma': self.charisma, 'items': self.items}
+        hero = {'name': self.name, 'power': self.power, 'agile': self.agile, 'durability': self.durability, 'iq': self.iq,
+                'intuition': self.intuition, 'charisma': self.charisma, 'items': get_hero_items('close')}
         return hero
 
 
 if __name__ == '__main__':
-    a = Hero(get_name())
+    a = Hero(get_name('human'))
     print(a.get_hero())
--- test_heroes.py
+++ test_heroes.py
@@ -1,5 +1,5 @@
 import unittest
-from heroes import Hero, get_name
+from heroes import Hero, get_name, get_hero_items
 from unittest import expectedFailure
 
 
@@ -20,6 +20,13 @@ class AccountTestCase(unittest.TestCase):
         self.assertIsInstance(hero, dict)
 
     def test_two_heroes_created_with_random_name(self):
-        hero1 = Hero(get_name())
-        hero2 = Hero(get_name())
+        hero1 = Hero(get_name('dwarf'))
+        hero2 = Hero(get_name('dwarf'))
         self.assertNotEqual(hero1, hero2)
+
+    def test_random_list_of_item_for_each_hero(self):
+        items_list1 = Hero(get_hero_items('close'))
+        print(items_list1)
+        items_list2 = Hero(get_hero_items('close'))
+        print(items_list2)
+        self.assertNotEqual(items_list1, items_list2)
