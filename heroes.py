99366d77e5454af53e0be77dfde88168d01b5623
Przemysław Hinca <pshemekhinca@gmail.com>
2020-12-28 17:26:57 +0100

    Correct tests

--- heroes.py
+++ heroes.py
@@ -8,22 +8,6 @@ def get_name(race):
         return name
 
 
-def get_hero_items(distance):
-    if distance == 'close':
-        kind1, kind2 = 'close', 'distance'
-    else:
-        kind1, kind2 = 'distance', 'close'
-
-    with open(f'txt_files/{kind1}_weapon.txt') as k1:
-        main_weapon = k1.read().splitlines()
-        first_weapon = list(random.sample(main_weapon, 2))
-    with open(f'txt_files/{kind2}_weapon.txt') as k2:
-        side_weapons = k2.read().splitlines()
-        secondary_weapon = list(random.sample(side_weapons, 1))
-
-    return first_weapon + secondary_weapon
-
-
 class Hero:
     def __init__(self, name: str, power=10, agile=10, durability=10, iq=10, intuition=10, charisma=10):
         self.name = name
@@ -38,11 +22,25 @@ class Hero:
     def get_hero(self):
         hero = {'name': self.name, 'power': self.power, 'agile': self.agile, 'durability': self.durability,
                 'iq': self.iq,
-                'intuition': self.intuition, 'charisma': self.charisma, 'items': get_hero_items('distance')}
+                'intuition': self.intuition, 'charisma': self.charisma, 'items': self.get_hero_items('close')}
         return hero
+
+    def get_hero_items(self, distance):
+        if distance == 'close':
+            kind1, kind2 = 'close', 'distance'
+        else:
+            kind1, kind2 = 'distance', 'close'
+
+        with open(f'txt_files/{kind1}_weapon.txt') as k1:
+            main_weapon = k1.read().splitlines()
+            first_weapon = list(random.sample(main_weapon, 2))
+        with open(f'txt_files/{kind2}_weapon.txt') as k2:
+            side_weapons = k2.read().splitlines()
+            secondary_weapon = list(random.sample(side_weapons, 1))
+
+        return first_weapon + secondary_weapon
 
 
 if __name__ == '__main__':
     a = Hero(get_name('elf'))
     print(a.get_hero())
-
--- test_heroes.py
+++ test_heroes.py
@@ -1,30 +1,32 @@
 import unittest
-from heroes import Hero, get_name, get_hero_items
+from heroes import Hero, get_name
 from unittest import expectedFailure
 
 
 class AccountTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.name = 'Gromek'
+        self.race = 'elf'
+        sample = Hero(get_name(self.race))
+        sample2 = Hero(get_name(self.race))
         self.power = 10
         self.agile = 10
-        self.armor = 10
+        self.durability = 10
         self.iq = 10
         self.intuition = 10
         self.charisma = 10
+        self.hero = sample.get_hero()
+        self.hero2 = sample2.get_hero()
 
     def test_hero_instance_created_as_dictionary(self):
-        sample = Hero(self.name)
-        hero = sample.get_hero()
-        self.assertIsInstance(hero, dict)
+        self.assertIsInstance(self.hero, dict)
 
-    def test_two_heroes_created_with_random_name(self):
-        hero1 = Hero(get_name('dwarf'))
-        hero2 = Hero(get_name('dwarf'))
-        self.assertNotEqual(hero1, hero2)
+    def test_heroes_created_with_random_name(self):
+        name1 = self.hero['name']
+        name2 = self.hero2['name']
+        self.assertNotEqual(name1, name2)
 
     def test_random_list_of_item_for_each_hero(self):
-        items_list1 = Hero(get_hero_items('close'))
-        items_list2 = Hero(get_hero_items('close'))
+        items_list1 = self.hero['items']
+        items_list2 = self.hero2['items']
         self.assertNotEqual(items_list1, items_list2)
