3c685568ff617cd0da71e0b42aefeee4245e3362
Przemysław Hinca <pshemekhinca@gmail.com>
2020-12-31 08:46:25 +0100

    Hero creates from CSV files instead txt.

--- heroes.py
+++ heroes.py
@@ -2,10 +2,11 @@ import random
 import csv
 
 
-def get_hero_of_race(x):
-    with open(f'txt_files/race_names.csv', 'r') as f:
+def get_hero_of_race(col_numb):
+    with open('txt_files/race_names.csv', 'r') as f:
         reader = csv.reader(f)
-        name = random.choice([row[x] for row in reader])
+        name = random.choice([row[col_numb] for row in reader])
+        print(name)
         return name
 
 
@@ -32,22 +33,23 @@ class Hero:
 
     def get_hero_items(self):
         if self.race == 'dwarf' or self.race == 'human':
-            kind1, kind2 = 'close', 'distance'
+            kind1, kind2 = 0, 1
         else:
-            kind1, kind2 = 'distance', 'close'
+            kind1, kind2 = 1, 0
 
-        with open(f'txt_files/{kind1}_weapon.txt') as k1:
-            main_weapon = k1.read().splitlines()
-            first_weapon = list(random.sample(main_weapon, 2))
-        with open(f'txt_files/{kind2}_weapon.txt') as k2:
-            side_weapons = k2.read().splitlines()
-            secondary_weapon = list(random.sample(side_weapons, 1))
-        return first_weapon + secondary_weapon
+        with open('txt_files/weapons.csv', 'r') as weapon:
+            reader = csv.reader(weapon)
+            main_weapon = random.sample([row[kind1] for row in reader], 2)
+        with open('txt_files/weapons.csv', 'r') as weapon:
+            reader = csv.reader(weapon)
+            side_weapon = random.sample([row[kind2] for row in reader], 1)
+        return main_weapon + side_weapon
 
 
 if __name__ == '__main__':
     create_kind = int(input("\nWhat race you choose for your hero: \n1. Human\n2. Elf\n3. Dwarf\n4. Shifter"
                             "\n\t\t\tpick the number:"))
     race = {1: 'human', 2: 'elf', 3: 'dwarf', 4: 'shifter'}
+
     sample_hero = Hero(race[create_kind], get_hero_of_race(create_kind - 1))
     print(sample_hero.get_hero())
