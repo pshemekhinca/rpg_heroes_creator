005c606f53379382acf2ca0762fb72331ea8b90b
Przemysław Hinca <pshemekhinca@gmail.com>
2020-12-31 08:05:41 +0100

    CSV name reader added.

--- heroes.py
+++ heroes.py
@@ -1,15 +1,17 @@
 import random
+import csv
 
 
-def get_hero_of_race(race):
-    with open(f'txt_files/name_{race}.txt') as f:
-        names = f.read().splitlines()
-        name = random.choice(names)
+def get_hero_of_race(x):
+    with open(f'txt_files/race_names.csv', 'r') as f:
+        reader = csv.reader(f)
+        name = random.choice([row[x] for row in reader])
         return name
 
 
 class Hero:
     """ Creates hero of given race with random name and random items set """
+
     def __init__(self, race: str, name: str, power=10, agile=10, durability=10, intellect=10, intuition=10,
                  charisma=10):
         self.race = race
@@ -44,7 +46,8 @@ class Hero:
 
 
 if __name__ == '__main__':
-    # create_kind = input("What race do you want to have for your hero: ")
-    create_kind = 'shifter'
-    sample_hero = Hero(create_kind, get_hero_of_race(create_kind))
+    create_kind = int(input("\nWhat race you choose for your hero: \n1. Human\n2. Elf\n3. Dwarf\n4. Shifter"
+                            "\n\t\t\tpick the number:"))
+    race = {1: 'human', 2: 'elf', 3: 'dwarf', 4: 'shifter'}
+    sample_hero = Hero(race[create_kind], get_hero_of_race(create_kind - 1))
     print(sample_hero.get_hero())
