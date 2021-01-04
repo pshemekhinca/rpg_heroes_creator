f5945f431906587cd311c0cf9f644dcf6b2c5b18
Przemysław Hinca <pshemekhinca@gmail.com>
2021-01-04 16:01:36 +0100

    While loop added.

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ intro_quest = input('Would You like to add hero/es?\n--> "Y" for yes or "q" to q
 while intro_quest != 'q':
     race = {1: 'human', 2: 'elf', 3: 'dwarf', 4: 'shifter'}
     pick_race = int(input("\nWhat race you choose for your hero: \n1. Human\n2. Elf\n3. Dwarf\n4. Shifter"
-                          "\n\t\tpick the race number:"))
+                          "\n\t\tpick the race number (1-4):"))
+
+    while pick_race not in range(1, 5):
+        pick_race = int(input('Pick correct number 1-4: '))
+
     heroes_qty = int(input(f"\nHow many of {race[pick_race]}s do you want to have? -> "))
 
-    for i in range(heroes_qty):
-        sample_hero = Hero(race[pick_race])
-        print(sample_hero.get_hero())
+    sample_team = CreateTeam('SampleTeam', race[pick_race], heroes_qty)
+    print(sample_team)
 
     intro_quest = input('Would You like to add next hero/es?\n--> "Y" for yes or "q" to quit')
