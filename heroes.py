2bef20965fbed4934b175c6aa28447403ebea420
Przemysław Hinca <pshemekhinca@gmail.com>
2021-01-07 17:21:49 +0100

    Getting names from json file.

--- heroes.py
+++ heroes.py
@@ -1,6 +1,25 @@
 import random
 import csv
-from pprint import pprint
+import json
+from itertools import chain
+
+
+def file_to_write():
+    with open('db_files/names_race.json', 'w') as f:
+        reader = json.load(f)
+        return reader
+
+
+def file_to_read_name():
+    with open('db_files/names_race.json', 'r') as f:
+        writer = json.load(f)
+        return writer
+
+
+def file_to_read_weapon():
+    with open('db_files/weapons.json', 'r') as weapon:
+        reader = json.load(weapon)
+        return reader
 
 
 class Hero:
@@ -18,34 +37,44 @@ class Hero:
         self.items = self.get_hero_items()
 
     def get_name_of_race(self):
-        with open('txt_files/race_names.csv', 'r') as f:
-            reader = csv.DictReader(f)
-            name = random.choice([names[self.race] for names in reader])
-            return name
+        reader = file_to_read_name()
+        name = random.choice(list(reader[self.race].values()))
+        return name
 
     def get_hero(self):
         one_hero = {'name': self.name, 'race': self.race, 'power': self.power,
-                    'agile': self.agile,
-                    'durability': self.durability, 'intellect': self.intellect, 'intuition': self.intuition,
-                    'charisma': self.charisma, 'items': self.items}
+                    'agile': self.agile, 'durability': self.durability, 'intellect': self.intellect,
+                    'intuition': self.intuition, 'charisma': self.charisma, 'items': self.items}
         return one_hero
+
+    # def get_hero_items(self):
+    #     if self.race == 'dwarf' or self.race == 'human':
+    #         kind1, kind2 = 0, 1
+    #     else:
+    #         kind1, kind2 = 1, 0
+    #     with open('db_files/weapons.csv', 'r') as weapon:
+    #         reader = csv.reader(weapon)
+    #         main_weapon = random.sample([row[kind1] for row in reader], 2)
+    #     with open('db_files/weapons.csv', 'r') as weapon:
+    #         reader = csv.reader(weapon)
+    #         side_weapon = random.sample([row[kind2] for row in reader], 1)
+    #     return main_weapon + side_weapon
 
     def get_hero_items(self):
         if self.race == 'dwarf' or self.race == 'human':
             kind1, kind2 = 0, 1
         else:
             kind1, kind2 = 1, 0
-        with open('txt_files/weapons.csv', 'r') as weapon:
+        with open('db_files/weapons.csv', 'r') as weapon:
             reader = csv.reader(weapon)
             main_weapon = random.sample([row[kind1] for row in reader], 2)
-        with open('txt_files/weapons.csv', 'r') as weapon:
+        with open('db_files/weapons.csv', 'r') as weapon:
             reader = csv.reader(weapon)
             side_weapon = random.sample([row[kind2] for row in reader], 1)
         return main_weapon + side_weapon
 
-
 class CreateTeam(Hero):
-    """ Creates heroes team of given race"""
+    """ Creates heroes team of given race under the chosen name"""
 
     def __init__(self, team_name: str, race: str, heroes_no: int):
         self.team_name = team_name
@@ -59,16 +88,17 @@ class CreateTeam(Hero):
 
     def __repr__(self):
         output = "\n".join(map(str, self.get_team_of_race()))
-        # output = "\n".join(map(str, self.team_list))
         return f"\n{self.team_name}:\n{output}"
-        # for i, val in enumerate(self)
+
 
 if __name__ == '__main__':
-    create_kind = 4
+    create_kind = 3
     race = {1: 'human', 2: 'elf', 3: 'dwarf', 4: 'shifter'}
     race_choice = race[create_kind]
-
     many_heroes = 3
 
-    sample_team = CreateTeam('SampleTeam', race_choice, many_heroes)
-    print(sample_team)
+    sample_h = Hero(race_choice)
+    print(sample_h.get_hero())
+    #
+    # sample_team = CreateTeam('SampleTeam', race_choice, many_heroes)
+    # print(sample_team)
