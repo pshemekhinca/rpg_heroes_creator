a93300bbb5533ea958061016b08a9e1c727b1876
Przemysław Hinca <pshemekhinca@gmail.com>
2021-01-08 14:15:07 +0100

    Added color classes.

---
+++ colors.py
@@ -0,0 +1,20 @@
+class FColor:
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    MAGENTA = '\033[95m'
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+
+    BOLD = '\033[1m'
+    UNDERL = '\033[4m'
+    END = '\033[0m'
+
+class BColor:
+    BLUE = '\033[104m'
+    CYAN = '\033[106m'
+    GREEN = '\033[102m'
+    MAGENTA = '\033[105m'
+    RED = '\033[101m'
+    YELLOW = '\033[103m'
+    END = '\033[0m'
