ad887865693c264206b146596280c21b2290d5d0
Przemysław Hinca <pshemekhinca@gmail.com>
2021-01-08 14:15:55 +0100

    Updated

--- test_heroes.py
+++ test_heroes.py
@@ -18,7 +18,7 @@ def hero2():
 @pytest.fixture()
 def race_team(hero):
     team_members_nmb = 3
-    test_team = CreateTeam('Test Name', hero['race'], team_members_nmb)
+    test_team = CreateTeam(hero['race'], team_members_nmb)
     race_team = test_team.get_team_of_race()
     return race_team
 
